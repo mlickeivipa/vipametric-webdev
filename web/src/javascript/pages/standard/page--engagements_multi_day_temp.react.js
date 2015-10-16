a12527db2eff14c8ff66192b05580f3de7577a5d
Scott Benes <sbenes@vipasolutions.com>
2015-10-15 21:27:42 -0500

    Multi day engagement temp updates

--- web/src/javascript/pages/standard/page--engagements_multi_day_temp.react.js
+++ web/src/javascript/pages/standard/page--engagements_multi_day_temp.react.js
@@ -25,15 +25,22 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Coupons Given Out</span>
-												<div ><div className="prop value-component" ><span className="val" >1100</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >1,000</span></div>
+													<div className="prop value-component" ><span className="val" >1,100</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Coupons Redeemed</span>
-												<div ><div className="prop value-component" ><span className="val" >95</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >100</span></div>
+													<div className="prop value-component" ><span className="val" >95</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -52,19 +59,29 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Giveaways</span>
-												<div ><div className="prop value-component" ><span className="val" >1500</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >1,000</span></div>
+													<div className="prop value-component" ><span className="val" >1,500</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Emails Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >650</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >400</span></div>
+													<div className="prop value-component" ><span className="val" >600</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Phone Numbers Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >500</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >300</span></div>
+													<div className="prop value-component" ><span className="val" >320</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -83,15 +100,22 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of T-shirts Given Away</span>
-												<div ><div className="prop value-component" ><span className="val" >1500</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >1,000</span></div>
+													<div className="prop value-component" ><span className="val" >1,000</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Photos</span>
-												<div ><div className="prop value-component" ><span className="val" >64</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >64</span></div>
+													<div className="prop value-component" ><span className="val" >50</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -155,15 +179,22 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of iPad games completed</span>
-												<div ><div className="prop value-component" ><span className="val" >278</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >300</span></div>
+													<div className="prop value-component" ><span className="val" >278</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Social Media Shares</span>
-												<div ><div className="prop value-component" ><span className="val" >92</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >100</span></div>
+													<div className="prop value-component" ><span className="val" >92</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -182,19 +213,29 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Giveaways</span>
-												<div ><div className="prop value-component" ><span className="val" >1500</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >1,500</span></div>
+													<div className="prop value-component" ><span className="val" >1,500</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Emails Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >682</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >800</span></div>
+													<div className="prop value-component" ><span className="val" >682</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Phone Numbers Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >581</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >500</span></div>
+													<div className="prop value-component" ><span className="val" >481</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -213,19 +254,29 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Giveaways</span>
-												<div ><div className="prop value-component" ><span className="val" >100</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >1,000</span></div>
+													<div className="prop value-component" ><span className="val" >1,000</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Emails Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >423</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >400</span></div>
+													<div className="prop value-component" ><span className="val" >423</span></div>
+												</div>
 											</div>
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Phone Numbers Captured</span>
-												<div ><div className="prop value-component" ><span className="val" >210</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >300</span></div>
+													<div className="prop value-component" ><span className="val" >210</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -244,11 +295,15 @@ jQuery(function($) {
 							<div className="miwt-collapsable-content">
 								<div className="prop-body" >
 									<div className="prop-group kpi" >
-										<div className="prop-group-title" >KPIs:</div>
+										<div className="prop-group-goal-title" ><span className="goal-title">Goals</span><span className="goal-actuals">Actuals</span></div>
+										<div className="prop-group-title" >&nbsp;</div>
 										<div className="metric-group props" >
 											<div className="metric-container numeric-metric BrandImpressions prop" >
 												<span  className="label-component"># of Surveys completed</span>
-												<div ><div className="prop value-component" ><span className="val" >762</span></div></div>
+												<div >
+													<div className="prop value-component goal-component" ><span className="val" >750</span></div>
+													<div className="prop value-component" ><span className="val" >762</span></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -261,12 +316,132 @@ jQuery(function($) {
 		}
 	});
 
-	$('.section.market-intelligence').each(function() {
-		var $con = $(this);
 
-		React.render(
-			<MultiDayEngagementData />,
-			this
-		);
-	});
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function tempUpdates(context) {
+		var $context = $(context);
+
+
+		$context.find('.section.goals, .section.market-intelligence').wrapAll('<div class="primary-content" />');
+
+		var surveys = [
+			{
+				name: "Pre-Event Survey",
+				link: '/demo/survey',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "RSVP",
+				link: '/demo/survey',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "In-Market Survey",
+				link: '/demo/survey',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "Post Event Survey",
+				link: '/demo/survey',
+				csv: '/somefile.csv'
+			}
+		];
+		var surveyHtml = $.map(surveys, function(el, idx) {
+			return [
+				'<div class="survey-item">',
+				'<div class="survey-item-header">',
+				'<a href="' + el.link + '" class="survey-item-name" title="Go to external survey">' + el.name + '</a>',
+				'</div>',
+				'<div class="survey-item-info">',
+				'<div class="survey-item-data">',
+				'<span class="survey-item-data-count">' + getRandomInt(50, 150) + '</span>',
+				'<span class="survey-item-data-text">Entries</span>',
+				'<a class="btn btn-glyph-only btn-xsmall btn-download" href="/_resources/dyn/files/35859z271659/_fn/metric-data.csv" title="Download Data"><span class="btn-text">Download Data</span></a>',
+				'</div>',
+				'</div>',
+				'</div>'
+			].join('');
+		}).join('');
+
+		$([
+			'<div class="section surveys">',
+			'<div class="section-header">Surveys</div>',
+			'<div class="section-content">',
+			'<div class="survey-items">',
+			surveyHtml,
+			'</div>',
+			'</div>',
+			'</div>'
+		].join(''))
+			.prependTo($context.find('.primary-content'));
+
+		var leads = [
+			{
+				name: "Emails",
+				css: 'lead-email',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "Phone Numbers",
+				css: 'lead-phone',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "Social Accounts",
+				css: 'lead-social',
+				csv: '/somefile.csv'
+			},
+			{
+				name: "Addresses",
+				css: 'lead-address',
+				csv: '/somefile.csv'
+			}
+		];
+
+		var leadHtml = $.map(leads, function(el, idx) {
+			return [
+				'<div class="lead ' + el.css + '">',
+				'<div class="lead-title">' + el.name + '</div>',
+				'<div class="lead-count">' + getRandomInt(100, 250) + '</div>',
+				'<a class="btn btn-glyph-only btn-xsmall btn-download" href="/_resources/dyn/files/35859z271659/_fn/metric-data.csv"  title="Download Data"><span class="btn-text">Download</span></a>',
+				'</div>'
+			].join('');
+		}).join('');
+
+		$([
+			'<div class="section leads">',
+			'<div class="section-header">Leads</div>',
+			'<div class="section-content">',
+			'<div class="lead-summary">',
+			leadHtml,
+			'</div>',
+			'</div>',
+			'</div>'
+		].join(''))
+			.insertBefore($context.find('.section.goals'));
+
+
+		$context.find('.section.goals').remove();
+		$context.find('.user-management').remove();
+	}
+
+	if ($('.multiday-example-box1').length) {
+
+		$('.section.market-intelligence').each(function() {
+			var $con = $(this);
+
+			React.render(
+				<MultiDayEngagementData />,
+				this
+			);
+		});
+
+
+		$('.miwt-form').each(function() {
+			tempUpdates(this);
+		});
+	}
 });
