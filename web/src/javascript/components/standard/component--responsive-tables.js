83f449e44929fc811a32e59bfb166a0b653cd89a
Kaitlyn Noone <knoone@vipasolutions.com>
2016-02-12 14:25:57 -0600

    Updates to responsive table initialization file

--- web/src/javascript/components/standard/component--responsive-tables.js
+++ web/src/javascript/components/standard/component--responsive-tables.js
@@ -1,11 +1,24 @@
 jQuery(function($){
-    var $tables = $('table.miwt-table');
+    var $forms = $("form");
 
-    function init() {
+    function init(context) {
+        var $con = $(context);
+        var $tables = $con.find('table.miwt-table');
+
         $tables.each(function(idx, table){
             $(table).stacktable();
         })
     }
 
-    init();
+
+
+    $forms.each(function() {
+        if (!this.submit_options) {
+            this.submit_options = {};
+        }
+
+        this.submit_options.postUpdate = function() {
+            init(this);
+        };
+    });
 });
